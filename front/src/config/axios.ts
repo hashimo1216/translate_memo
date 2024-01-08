2737342beb1af6c0806d29ff4576ac70cbec574f
Yuto Hashimoto <69133317+hashimo1216@users.noreply.github.com>
2024-01-08 14:12:41 +0900

    modify axios.ts

--- front/src/config/axios.ts
+++ front/src/config/axios.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production'
-    ? 'https://translate-memo-5cf100b7d182.herokuapp.com'
-    : 'http://localhost:8080';
+const baseURL = 'https://translate-memo-api.onrender.com'
 
 export default axios.create({
   baseURL,
