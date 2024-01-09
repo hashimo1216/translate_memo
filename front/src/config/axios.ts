6bd04372bfd048acdd3184f7bbd8503c9f66ce6a
Yuto Hashimoto <69133317+hashimo1216@users.noreply.github.com>
2024-01-09 16:12:10 +0900

    commit

--- front/src/config/axios.ts
+++ front/src/config/axios.ts
@@ -5,4 +5,7 @@ const baseURL = 'https://translate-memo-api.onrender.com'
 export default axios.create({
   baseURL,
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
